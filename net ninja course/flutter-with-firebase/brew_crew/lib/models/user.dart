5231459a0ff616204592635d04d7973211ecc6c0
Hardik Shrestha <hardik.ho@gmail.com>
2020-06-14 11:30:43 -0400

    Fourth Commit

---
+++ net ninja course/flutter-with-firebase/brew_crew/lib/models/user.dart
@@ -0,0 +1,9 @@
+// CREATING CUSTOM USER FROM FIRBASE USER
+
+class User {
+
+  final String uid;
+
+  User({this.uid});
+
+}
--- net ninja course/flutter-with-firebase/brew_crew/lib/services/auth.dart
+++ net ninja course/flutter-with-firebase/brew_crew/lib/services/auth.dart
@@ -1,8 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change the user stream | Set up the stream for anytime a user signs in/ out
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+    // .map((FirebaseUser user) => _userFromFirebaseUser(user));
+  }
 
   // sign in anon
   Future signInAnon () async {
@@ -10,7 +22,7 @@ class AuthService {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }
     
     catch (e) {
