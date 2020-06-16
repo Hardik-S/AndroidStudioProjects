22397f829eaf9ba6b9182efa2d7fcdfd16503088
Hardik Shrestha <hardik.ho@gmail.com>
2020-06-15 22:52:51 -0400

    Images + Testing Implementation

--- net ninja course/flutter-with-firebase/brew_crew/lib/screens/home/home.dart
+++ net ninja course/flutter-with-firebase/brew_crew/lib/screens/home/home.dart
@@ -47,7 +47,17 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        body: BrewList(),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bkg.jpg'),
+              fit: BoxFit.cover,
+              
+            ),
+          ),
+          child: BrewList()
+        
+        ),
       ),
     );
   }
